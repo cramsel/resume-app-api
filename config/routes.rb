58fa08e1b4bc78550c9f259e537761ccdace9897
cramsel <Luxicorde@gmail.com>
2022-03-07 19:39:02 -0600

    calum pull test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # haha david test
+  # calum test
   # Defines the root path route ("/")
   # root "articles#index"
 end
