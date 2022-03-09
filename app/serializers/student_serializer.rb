a101fe83251fdeb305feefa15d1373c9e88b0a2c
David Mandolini <damandolini@gmail.com>
2022-03-08 19:57:47 -0600

    fix linkedin url

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,5 +1,5 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :password, :phone_number, :short_bio, :linked_url, :twitter_handle, :personal_blog_url, :resume_url, :github_url, :photo_url
+  attributes :id, :first_name, :last_name, :email, :password, :phone_number, :short_bio, :linkedin_url, :twitter_handle, :personal_blog_url, :resume_url, :github_url, :photo_url
   has_many :skills
   has_many :educations
   has_many :projects
