c233ef9f9b010c6886e9e336d6dc39ef213a81fa
cramsel <Luxicorde@gmail.com>
2022-03-08 20:12:55 -0600

    Update seed/schema to use password instead of password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_021953) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_020821) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,7 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_021953) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password_digest"
+    t.string "password"
     t.string "phone_number"
     t.text "short_bio"
     t.string "linkedin_url"
