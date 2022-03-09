8da6fa62af6bfa11d73488767726ffedeeb2b4ca
David Mandolini <damandolini@gmail.com>
2022-03-08 19:54:59 -0600

    add belongs to association

--- app/serializers/education_serializer.rb
+++ app/serializers/education_serializer.rb
@@ -1,3 +1,4 @@
 class EducationSerializer < ActiveModel::Serializer
   attributes :id, :student_id, :start_date, :end_date, :degree, :university_name, :details
+  belongs_to :student
 end
--- app/serializers/skill_serializer.rb
+++ app/serializers/skill_serializer.rb
@@ -1,3 +1,4 @@
 class SkillSerializer < ActiveModel::Serializer
   attributes :id, :student_id, :skill_name
+  belongs_to :student
 end
